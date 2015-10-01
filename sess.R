0eff8a1f60e2ce11ee946465041e98bd364ef68a
liztwb <github@timbailey.org.uk>
2015-10-01 13:30:54 +0100

    basic working effort

--- sess.R
+++ sess.R
@@ -1,3 +1,15 @@
 library (devtools)
 
 install_github('geoffjentry/twitteR') # install 'twitteR' 
+library(twitteR)
+cons_key <- 'lHhFjOw8xcpirgR2eNfEjIKsl'
+cons_secret <- 'aPSaE45vOj0weZ2bdYzjeFipRjgTaEFUW81Oh9hsz5KsMAwmm1'
+acc_tok <- '93018926-FZE34lfPmHrY7tvZAfFGbk4xkL5KbwQkBeP01ZzDb'
+acc_secret <- 'GdWwqrKKuBe8lCdD3zW8Zzc41McvMiM2TxIRS7wJ2Uo3D'
+
+setup_twitter_oauth (cons_key, cons_secret, acc_tok, acc_secret)
+homeTimeline (n=15)
+user <- getUser("KimKardashian")
+user$followersCount
+
+user$location
